0fd8e4c3fc623d0ac2b4235a7e3febd0cc2bfdec
Melissa Wallace <mhk33@cornell.edu>
2013-05-30 16:58:03 +0000

    try to fix error being thrown for context variable
    git-svn-id: https://svn.library.cornell.edu/efraimrackertheme/trunk@94 9597bc91-b0d9-4081-9644-6f6672fb8dfc

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -120,7 +120,11 @@
 
 <header>
   <div class="container">
-    <img src="/sites/all/themes/bootstrap_racker/images/header-<?php print $section_title; ?>.jpg" alt="">
+    <?php  if (!empty($section_title)): ?>
+      <img src="/sites/all/themes/bootstrap_racker/images/header-<?php print $section_title; ?>.jpg" alt="">
+    <?php else: ?>
+      <img src="/sites/all/themes/bootstrap_racker/images/header-artworks.jpg" alt=""> 
+    <?php endif; ?>
   </div>
 </header>
 
