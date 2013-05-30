43ad254a7d6c638a049f62ce5a73e28a918a5e9e
Melissa Wallace <mhk33@cornell.edu>
2013-05-30 14:58:14 +0000

    add context variable for banner image
    git-svn-id: https://svn.library.cornell.edu/efraimrackertheme/trunk@87 9597bc91-b0d9-4081-9644-6f6672fb8dfc

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -120,7 +120,7 @@
 
 <header>
   <div class="container">
-    <img src="/sites/all/themes/bootstrap_racker/images/header-about.jpg" alt="">
+    <img src="/sites/all/themes/bootstrap_racker/images/header-<?php print $section_title; ?>.jpg" alt="">
     <p class="caption">Caption goes here</p>
   </div>
 </header>
