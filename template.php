b5c271190cc2d0330a38b9717935fd72170cda3c
Melissa Wallace <mhk33@cornell.edu>
2013-06-05 17:06:26 +0000

    try to customize search form
    git-svn-id: https://svn.library.cornell.edu/efraimrackertheme/trunk@124 9597bc91-b0d9-4081-9644-6f6672fb8dfc

--- template.php
+++ template.php
@@ -3,3 +3,19 @@
 /**
  * @file template.php
  */
+
+<?php
+/**
+ * Override bootstrap search form button
+ */
+function bootstrap_bootstrap_search_form_wrapper(&$variables) {
+  $output = '<div class="input-append">';
+  $output .= $variables['element']['#children'];
+  $output .= '<button type="submit">';
+  $output .= '<i class="icon-search"></i>';
+  $output .= '<span class="element-invisible">' . t('Search') . '</span>';
+  $output .= '</button>';
+  $output .= '</div>';
+  return $output;
+ }
+?>
