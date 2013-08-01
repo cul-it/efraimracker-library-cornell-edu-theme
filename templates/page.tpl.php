f07bfe43de0aba7e41d046a532190ccf30661f96
Melissa Wallace <mhk33@cornell.edu>
2013-08-01 18:29:56 +0000

    test creating a collapsible search box for mobile
    git-svn-id: https://svn.library.cornell.edu/efraimrackertheme/trunk@212 9597bc91-b0d9-4081-9644-6f6672fb8dfc

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -92,14 +92,14 @@
 
 <nav class="navbar">
   <div class="container">
-    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+    <btn class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
-    </a>
-    <a class="btn btn-search" data-toggle="collapse" data-target=".search-collapse">
+    </btn>
+    <btn class="btn btn-search" data-toggle="collapse" data-target=".search-collapse">
       <i class="icon-search"></i>
-    </a>
+    </btn>
     <div class="row">
       <div class="span6">
         <?php if ($site_name): ?>
